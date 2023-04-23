e6c1d55fb8e4bd692126d05133716c727fb53add
Timur <timengalytchev@mail.ru>
2023-04-23 19:33:17 +0300

    ui without workin contracts

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,13 @@
+// https://eth-mainnet.g.alchemy.com/v2/-6MhhwuFUEltMG2JKMnw102o6MyNeuN6
+
+require('@nomiclabs/hardhat-waffle')
+
+module.exports = {
+  solidity: '0.8.9',
+  networks: {
+    goerli:{
+      url: 'https://eth-mainnet.g.alchemy.com/v2/-6MhhwuFUEltMG2JKMnw102o6MyNeuN6',
+      accounts: ['50d7e2a32c3eab31b53cf8a0cb1231a385e448911beff7674eb1d26d864d815b']
+    }
+  }
+}
